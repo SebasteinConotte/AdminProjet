79e6e4cded6984b6894779e413cdff09246e949e
Aires David <david.airespimentel@gmail.com>
2019-05-26 13:22:59 +0200

    add

--- DB/script/init.sh
+++ DB/script/init.sh
@@ -5,7 +5,7 @@ service mysql start
 
 
 mysql -u root --password=test0155
-mysql --password=test0155 < /tmp/db_schema.sql
+mysql --password=test0155 < /tmp/db_shema.sql
 
 
 mysqladmin --password=test0155 shutdown
