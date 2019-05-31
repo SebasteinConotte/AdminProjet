412795ed9ca44a30d12d9f1bf2f4375aa58baa89
SebastienConotte <44473643+SebasteinConotte@users.noreply.github.com>
2019-05-31 20:55:32 +0200

    Utilisation de la nvelle DB

--- Web/b2B/html/action.php
+++ Web/b2B/html/action.php
@@ -1,19 +1,22 @@
 <?php
-$servername = "85.10.205.173";
-$username = "ephec_tp";
-$password = "ephec0155";
-$bdd = new PDO('mysql:host=85.10.205.173;dbname=db_ephec;charset=utf8', $username, $password);
-//echo "Connected successfully";
-
-$localite = $_POST['ville'];
+$servername = "51.77.203.87";
+$username = "admin";
+$password = "0I47Q(bxRIUC";
+$bdd = new PDO('mysql:host=51.77.203.87;dbname=db_ephec;charset=utf8', $username, $password);
+$localite = $_POST['cat'];
 $reponse = $bdd->prepare('SELECT * FROM CLIENT WHERE LOCALITE = ?');
 $reponse->execute(array($localite));
-$listeNom = '';
+$listeNom = '<style>table, td, tr {border: 1px solid black;border-collapse: collapse;padding: 15px;}</style><table><tr><th>NCLI</th><th>NOM</th><th>ADRESSE</th><th>LOCALITE</th><th>CAT</th><th>COMMANDES</th></tr>';
 while ($donnees = $reponse->fetch()) {
-    $listeNom .= '<li>'.$donnees['NOM'].'</li>';
+    $listeNom .= '<tr><td>'.$donnees[0].'</td><td>'.$donnees[1].'</td><td>'.$donnees[2].'</td><td>'.$donnees[3].'</td><td>'.$donnees[4].'</td>';
+    $test = $bdd->prepare('SELECT * FROM COMMANDE WHERE NCLI = ?');
+    $test->execute(array($donnees[0]));
+    $listeNom .= '<td>';
+    while ($donneesTest = $test->fetch()) {
+        $listeNom .= '<li>' . $donneesTest[0] .' '. $donneesTest[2] . '</li>';
+    }
+    $listeNom .= '</td></tr>';
 }
-$tab = array(
-    'nom' => '<ul>'.$listeNom.'</ul>'
-);
+$listeNom .= '</table>';
+echo $listeNom;
 
-echo $tab['nom'];
--- Web/b2B/html/index.php
+++ Web/b2B/html/index.php
@@ -1,25 +1,24 @@
 <?php
-
-$servername = "85.10.205.173";
-$username = "ephec_tp";
-$password = "ephec0155";
-$bdd = new PDO('mysql:host=85.10.205.173;dbname=db_ephec;charset=utf8', $username, $password);
+$servername = "51.77.203.87";
+$username = "admin";
+$password = "0I47Q(bxRIUC";
+$bdd = new PDO('mysql:host=51.77.203.87;dbname=db_ephec;charset=utf8', $username, $password);
 $reponse = $bdd->query('SELECT distinct LOCALITE FROM CLIENT');
-$listeVilles = array();
+$listeLocalite = array();
 while ($donnees = $reponse->fetch()) {
-    $listeVilles[] = $donnees['LOCALITE'];
+    $listeLocalite[] = $donnees['LOCALITE'];
 }
 ?>
 <html>
 <head>
-    <title>Nom et numéro de téléphone des membres</title>
+    <title>Zone de recherches</title>
 </head>
 <body>
 <form action="action.php" method="post">
-    <p>Ville de recherche:
-        <select name="ville" size="1">
-            <?php foreach ($listeVilles as $ville): ?>
-                <option> <?= $ville ?> </option>
+    <p>Sélectionnez la zone de recherche :
+        <select name="cat" size="1">
+            <?php foreach ($listeLocalite as $cat): ?>
+                <option> <?= $cat ?> </option>
             <?php endforeach ?>
         </select></p>
     <p><input type="submit" value="OK"></p>
