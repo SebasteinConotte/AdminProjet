7f15f79e823f8d88eb81a85b147ab40208754f21
SebastienConotte <44473643+SebasteinConotte@users.noreply.github.com>
2019-06-05 22:11:31 +0200

    updates

--- Web/b2B/html/action.php
+++ Web/b2B/html/action.php
@@ -6,16 +6,20 @@ $bdd = new PDO('mysql:host=51.77.203.87;dbname=db_ephec;charset=utf8', $username
 $localite = $_POST['cat'];
 $reponse = $bdd->prepare('SELECT * FROM CLIENT WHERE LOCALITE = ?');
 $reponse->execute(array($localite));
-$listeNom = '<style>table, td, tr {border: 1px solid black;border-collapse: collapse;padding: 15px;}</style><table><tr><th>NCLI</th><th>NOM</th><th>ADRESSE</th><th>LOCALITE</th></tr>';
+$listeNom = '<table><tr><th>NCLI</th><th>NOM</th><th>ADRESSE</th><th>LOCALITE</th></tr>';
 while ($donnees = $reponse->fetch()) {
     $listeNom .= '<tr><td>'.$donnees[0].'</td><td>'.$donnees[1].'</td><td>'.$donnees[2].'</td><td>'.$donnees[3].'</td></tr>';
 }
 $listeNom .= '</table>';
-echo $listeNom;
 ?>
 <html>
-<head></head>
+<head>
+    <style>
+        table, td, tr {border: 1px solid black;border-collapse: collapse;padding: 15px;}
+    </style>
+</head>
 <body>
+<?= $listeNom ?>
 <form action="action2.php" method="post">
     <p>
         INSERTION nouvelles données :<br>
