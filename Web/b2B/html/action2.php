e98aa330c6f6f319402f3d87b9ac8f2e8cc8c7b4
SebastienConotte <44473643+SebasteinConotte@users.noreply.github.com>
2019-06-05 22:55:46 +0200

    update insert

--- Web/b2B/html/action2.php
+++ Web/b2B/html/action2.php
@@ -3,13 +3,13 @@ $servername = "51.77.203.87";
 $username = "admin";
 $password = "0I47Q(bxRIUC";
 $bdd = new PDO('mysql:host=51.77.203.87;dbname=db_ephec;charset=utf8', $username, $password);
-$req = $bdd->prepare('INSERT INTO CLIENT(NCLI, NOM, ADRESSE, LOCALITE, CAT, COMPTE) VALUES(:ncli, :nom, :adresse, :localite, :cat, :compte)');
+$req = $bdd->prepare('INSERT INTO CLIENT(NCLI, NOM, ADRESSE, LOCALITE, CAT, COMPTE) VALUES(?,?,?,?,?,?)');
 $req->execute(array(
-    'ncli'=> $_POST['ncli'],
-    'nom' => $_POST['nom'],
-    'adresse' => $_POST['adresse'],
-    'localite' => $_POST['localite'],
-    'cat' => null,
-    'compte' => null
+    $_POST['ncli'],
+    $_POST['nom'],
+    $_POST['adresse'],
+    $_POST['localite'],
+    NULL,
+    0
 ));
 echo 'les données ont été ajoutée !';
